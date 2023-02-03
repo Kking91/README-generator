df5055b0ebe40e53a3da0b6a44dac616f6d8689a
Kiefer King <kiefydking@yahoo.com>
2023-02-03 13:54:56 -0600

    finished the code and added comments.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// Import the fs module to be able to write to the file system
 const fs = require("fs");
+
+// Import the inquirer module to prompt the user for input
 const inquirer = require("inquirer");
+
+// Import the generateMarkdown module to create the README content
 const generateMarkdown = require("./utils/generateMarkdown");
+
+// Define an array of questions to ask the user
 const questions = [
     {
       type: "input",
@@ -50,19 +57,28 @@ const questions = [
     }
   ];
 
+// Function to write the content to a file
 function writeToFile(fileName, data) {
+  // Use fs.writeFile to write the content to the file system
   fs.writeFile(fileName, data, function(err) {
+    // Check if there is an error
     if (err) {
+        // If there is an error, log it and return
       return console.log(err);
     }
+    // If it was successful, console log the message
     console.log("Successfully wrote to file!");
   });
 }
 
+// Function to start the prompt and write the file
 function init() {
+    //  Uses inquirer.prompt to ask the user questions and receive answers
   inquirer.prompt(questions).then(function(response) {
+    // Writes to the README1.md file and the content from the generateMarkdown
     writeToFile("README1.md", generateMarkdown(response));
   });
 }
 
+// Calls the init function to begin the program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,102 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Defines a function to render a license badge based on the license type
+function renderLicenseBadge(license) {
+  // If license type is MIT return the MIT license badge
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } 
+  // If license type is Apache 2.0 return the Apache 2.0 license badge
+  else if (license === "Apache 2.0") {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } 
+  // If license type is GPL 3.0 return the GPL 3.0 license badge
+  else if (license === "GPL 3.0") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } 
+  // If license type is unknown return an empty string
+  else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Defines a function to render a link to the license information based on the license type
+function renderLicenseLink(license) {
+  // If license type is MIT return the MIT license badge
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } 
+  // If license type is Apache 2.0 return the Apache 2.0 license badge
+  else if (license === "Apache 2.0") {
+    return "https://www.apache.org/licenses/LICENSE-2.0";
+  } 
+  // If license type is GPL 3.0 return the GPL 3.0 license badge
+  else if (license === "GPL 3.0") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } 
+  // If license type is unknown return an empty string
+  else {
+    return "";
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Defines a function to render a string indicating the license type
+function renderLicenseSection(license) {
+  // If license type is MIT return the MIT license badge
+  if (license === "MIT") {
+    return "This project is licensed under the MIT License.";
+  } 
+  // If license type is Apache 2.0 return the Apache 2.0 license badge
+  else if (license === "Apache 2.0") {
+    return "This project is licensed under the Apache 2.0 License.";
+  } 
+  // If license type is GPL 3.0 return the GPL 3.0 license badge
+  else if (license === "GPL 3.0") {
+    return "This project is licensed under the GPL 3.0 License.";
+  } 
+  // If license type is unknown return an empty string
+  else {
+    return "";
+  }
+}
+
+// This function generates a markdown file using the passed in data.
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## License
+
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.title)}
+
+## Questions
+For any questions, please contact me at [${data.email}](mailto:${data.email}) or through [GitHub](https://github.com/${data.github}).`;
 }
 
+// Exports the generateMarkdown function to be used in other modules.
 module.exports = generateMarkdown;
